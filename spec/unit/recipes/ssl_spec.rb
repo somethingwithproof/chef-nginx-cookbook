1a58839bc95fb3fcc46f109fd1c5fead1943fa16
Thomas Vincent <thomasvincent@gmail.com>
2026-01-09 02:13:32 -0800

    feat: nginx updates, modules, tests, CI
    - Update nginx.conf.erb with security headers (X-Frame-Options, X-Content-Type-Options,
      X-XSS-Protection, Referrer-Policy, Permissions-Policy)
    - Add SSL/TLS configuration with modern ciphers (TLS 1.2/1.3)
    - Add gzip and brotli compression support
    - Create ChefSpec unit tests for default, modules, and SSL recipes
    - Update CI workflow with security scanning job
    - Update README with comprehensive module installation examples
    - Add security headers documentation table

---
+++ spec/unit/recipes/ssl_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'nginx::default' do
+  context 'with SSL enabled on Ubuntu 22.04' do
+    platform 'ubuntu', '22.04'
+
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '22.04') do |node|
+        node.normal['nginx']['ssl']['enabled'] = true
+        node.normal['nginx']['ssl']['certificate'] = '/etc/ssl/certs/server.crt'
+        node.normal['nginx']['ssl']['certificate_key'] = '/etc/ssl/private/server.key'
+      end.converge(described_recipe)
+    end
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+
+    it 'installs nginx package' do
+      expect(chef_run).to install_package('nginx')
+    end
+
+    it 'enables nginx service' do
+      expect(chef_run).to enable_service('nginx')
+    end
+  end
+
+  context 'with HTTP/2 and TLS 1.3 on Ubuntu 24.04' do
+    platform 'ubuntu', '24.04'
+
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '24.04') do |node|
+        node.normal['nginx']['ssl']['enabled'] = true
+        node.normal['nginx']['ssl']['protocols'] = 'TLSv1.2 TLSv1.3'
+        node.normal['nginx']['http2']['enabled'] = true
+      end.converge(described_recipe)
+    end
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+  end
+end
